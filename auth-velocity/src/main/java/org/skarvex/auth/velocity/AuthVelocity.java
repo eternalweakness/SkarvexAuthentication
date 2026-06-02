803005f2a701eee3878e96bfa373e758290db34f
eternalweakness <sm.yatsunenko@outlook.com>
2026-06-02 15:58:44 +0200

    initialize project

---
+++ auth-velocity/src/main/java/org/skarvex/auth/velocity/AuthVelocity.java
@@ -0,0 +1,16 @@
+package org.skarvex.auth.velocity;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import org.slf4j.Logger;
+
+public class AuthVelocity {
+
+    @Inject private Logger logger;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+      // Plugin initialization logic goes here
+    }
+}
